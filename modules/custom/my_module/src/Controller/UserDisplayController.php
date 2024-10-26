4bbd1e9106388e51624138250d1fe62faf761b7d
devphp <contact.devphp@gmail.com>
2024-10-26 16:52:10 +0530

    deleted custom twig filter module

--- modules/custom/my_module/src/Controller/UserDisplayController.php
+++ modules/custom/my_module/src/Controller/UserDisplayController.php
@@ -1,14 +1,21 @@
 <?php
+
 namespace Drupal\my_module\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * User display class.
+ */
 class UserDisplayController extends ControllerBase {
+
+  /**
+   * Display User.
+   */
   public function displayUser() {
     $current_user = \Drupal::currentUser();
     $username = $current_user->getAccountName();
-    return [
-      '#markup' => 'Hello, ' . $username . '!',
-    ];
+    return ['#markup' => 'Hello, ' . $username . '!'];
   }
+
 }
